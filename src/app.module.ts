0c63f57c98c5c89a32143d230eb80ee02e3a2c6f
Ricardo Gutiérrez <rgutierrez@black-n-orange.com>
2020-06-19 10:56:45 -0500

    Proxy functions started

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProxyController } from './controller/proxy/proxy.controller';
+import { CompanyService } from './service/company/company.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProxyController],
+  providers: [AppService, CompanyService],
 })
 export class AppModule {}
